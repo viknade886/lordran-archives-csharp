0c6726843e6d4d5b914243ca4f6627448c919cb2
viknade886 <viknade886@gmail.com>
2026-03-12 16:40:28 +0200

    swagger

--- lordran-archives/Program.cs
+++ lordran-archives/Program.cs
@@ -66,11 +66,10 @@ using (var scope = app.Services.CreateScope())
     }
 }
 
-if (app.Environment.IsDevelopment())
-{
+
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+
 
 app.UseHttpsRedirection();
 app.UseCors("AllowAll");
